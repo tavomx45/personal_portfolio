83b07de02eca38b65d903b08cc48d9dbaf614c45
Gustavo Paredes <bryangustavoparedes@gmail.com>
2025-01-18 15:59:36 -0500

    main.js updated

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@
 	/*---------------------------------------------------- */
 	/* Preloader
 	------------------------------------------------------ */ 
-   $(window).load(function() {
+   $(window).on("load", function() {
 
       // will first fade out the loading animation 
     	$("#loader").fadeOut("slow", function(){
@@ -22,7 +22,7 @@
 
       });       
 
-  	})
+  	});
 
 
   	/*---------------------------------------------------- */
